b7fabea74c564a186c5436c61564c0dffbbe6f1e
Bruk Argaw <bkargaw@gmail.com>
2017-07-13 12:26:51 -0700

    added transitions to the app

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,5 @@
 // this makes my app as a unique variable in our application
-let artistControllers = angular.module('artistControllers', []);
+let artistControllers = angular.module('artistControllers', ['ngAnimate']);
 
 artistControllers.controller('ListController', ['$scope','$http',function MyController($scope, $http) {
   $http.get('./js/data.json').success(function (data) {
@@ -11,5 +11,17 @@ artistControllers.controller('DetailController', ['$scope','$http','$routeParams
   $http.get('./js/data.json').success(function (data) {
     $scope.artists =  data
     $scope.whichItem =  $routeParams.itemId
+
+    if($routeParams.itemId > 0){
+      $scope.preveItem = Number($routeParams.itemId) -1
+    }else{
+      $scope.preveItem = $scope.artists.length -1
+    }
+
+    if($routeParams.itemId < $scope.artists.length - 1){
+      $scope.nextItem = Number($routeParams.itemId) +1
+    }else{
+      $scope.nextItem = 0
+    }
   })
 }])
