14f38bf6699fc14e40517b392609a163d2d20194
Bruk Argaw <bkargaw@gmail.com>
2017-07-21 16:41:22 -0700

    finished with createing the conatactApp

--- ContactApp/js/contact.controller.js
+++ ContactApp/js/contact.controller.js
@@ -4,16 +4,66 @@
 
   function ContactCnt(contactDataSrv) {
     let self = this;
+    this.editMode = false;
+
+    this.deleteUser = function(){
+      console.log('delete called  with name' + this.selectedContact.name.first )
+      contactDataSrv.deleteUser(this.selectedContact)
+      .then(function () {
+        self.successMsg = 'User Data Successfully Updated!!'
+        self.selectedContact = {}
+      },
+        function () {
+          self.errorMsg = "There was an error, Please try again"
+        }
+      )
+    }
+    this.addContact = function() {
+      this.selectedContact = {
+        id: new Date().toTimeString()
+      }
+      this.editMode = true;
+    }
+
+    this.toggelEditMode = function () {
+      this.editMode = !this.editMode;
+    }
+
+    this.saveUser = function() {
+      if (this.editMode){
+        this.toggelEditMode()
+        contactDataSrv.updateUser(this.selectedContact)
+        .then(function () {
+          self.successMsg = 'User Data Successfully Updated!!'
+        },
+          function () {
+            self.errorMsg = "There was an error, Please try again"
+          }
+        )
+      }else {
+        contactDataSrv.createUser(this.selectedContact)
+        .then(function () {
+          self.successMsg = 'Contact Sucrcessfully Added!!'
+        },
+          function () {
+            self.errorMsg = "There was an error, Please try again"
+          }
+        )
+      }
+
+    }
 
     contactDataSrv.getContacts()
     .then(function(data){
       self.contacts = data;
       self.selectedContact = data[0];
     })
-    
-    this.selectedContact;
+
     this.selectContact =function (index) {
       this.selectedContact = this.contacts[index];
+      this.editMode = false;
+      this.successMsg = undefined;
+      this.errorMsg = undefined;
     }
   }
 })();
--- ContactApp/js/contactData.service.js
+++ ContactApp/js/contactData.service.js
@@ -5,13 +5,23 @@
 
     this.getContacts = function () {
       let promise = $http.get('http://localhost:3000/contacts')
-      .then(function(contacts){
-        return contacts.data;
+      .then(function(response){
+        return response.data;
       })
-      
       return promise;
     }
 
+    this.updateUser = function (userData) {
+      return $http.put('http://localhost:3000/contacts/' + userData.id, userData)
+    }
+
+    this.createUser = function (userData) {
+      return $http.post('http://localhost:3000/contacts/', userData)
+    }
+
+    this.deleteUser = function (userData) {
+      return $http.delete('http://localhost:3000/contacts/' + userData.id)
+    }
 
   })
 })()
